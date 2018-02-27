84f0bf2e6d8b670aef7015ece5cf514bfdac8b86
martin Berton <mberton@csumb.edu>
2018-02-27 04:51:36 +0000

    initial commit

--- Lab/Lab3/silverJack.php
+++ Lab/Lab3/silverJack.php
@@ -1,18 +1,14 @@
-<?php
-    include 'functions.php'
-?>
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html>
     <head>
         <title>Silverjack</title>
         <link rel="stylesheet" href="styles.css" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Audiowide|Fugaz+One|Vast+Shadow" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Bungee+Inline|Bungee+Shade" rel="stylesheet">  
     </head>
     <body>
         <h1>Silverjack</h1>
-        <?php
-            play();
-        ?>
-        
+        <div class='containerTitle'><p>PLAYER 1:</p><p>PLAYER 2:</p><p>PLAYER 3:</p><p>PLAYER 4:</p></div><div class = 'carte1'><img src='cards/clubs/10.png' class= 'player1 card1 '></img><img src='cards/clubs/7.png' class= 'player1 card2 '></img><img src='cards/hearts/12.png' class= 'player1 card3 '></img><img src='cards/hearts/2.png' class= 'player1 card4 '></img><img src='cards/hearts/8.png' class= 'player1 card5 '></img></div><div class = 'carte2'><img src='cards/spades/8.png' class= 'player2 card1 '></img><img src='cards/diamonds/7.png' class= 'player2 card2 '></img><img src='cards/clubs/12.png' class= 'player2 card3 '></img><img src='cards/hearts/11.png' class= 'player2 card4 '></img></div><div class = 'carte3'><img src='cards/spades/12.png' class= 'player3 card1 '></img><img src='cards/clubs/3.png' class= 'player3 card2 '></img><img src='cards/hearts/3.png' class= 'player3 card3 '></img><img src='cards/clubs/6.png' class= 'player3 card4 '></img><img src='cards/spades/4.png' class= 'player3 card5 '></img><img src='cards/diamonds/6.png' class= 'player3 card6 '></img><img src='cards/spades/1.png' class= 'player3 card7 '></img><img src='cards/diamonds/1.png' class= 'player3 card8 '></img></div><div class = 'carte4'><img src='cards/diamonds/5.png' class= 'player4 card1 '></img><img src='cards/clubs/9.png' class= 'player4 card2 '></img><img src='cards/clubs/8.png' class= 'player4 card3 '></img><img src='cards/hearts/6.png' class= 'player4 card4 '></img><img src='cards/hearts/10.png' class= 'player4 card5 '></img></div><div class = 'winner'> PLAYER 1 WINS 151!!!</div><div class='containerPoints'><p>39</p><p>38</p><p>36</p><p>38</p></div>        <form>
+             <input class="button" type="submit" value="Play Again"/>
+        </form>
     </body>
 </html>
